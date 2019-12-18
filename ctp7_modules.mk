78d2385e0c512f5c3f498f9b5029faa53c4915ea
Jared Sturdy <sturdy@cern.ch>
2019-12-18 17:18:42 +0100

    adapt and migrate extras

--- ctp7_modules.mk
+++ ctp7_modules.mk
@@ -82,7 +82,7 @@ TestSources  := $(wildcard $(PackageTestSourceDir)/*.cxx) $(wildcard $(PackageTe
 Dependencies := $(patsubst $(PackageSourceDir)/%.cpp, $(PackageObjectDir)/%.d, $(Sources))
 TargetObjects:= $(patsubst %.d,%.o,$(Dependencies))
 
-TargetLibraries:=memhub memory utils
+TargetLibraries:=memhub memory utils extras
 ifeq ($(Arch),x86_64)
 else
 TargetLibraries+=optical
