60216ac680f2d8b62a1531b1f3764058dfac9a6a
Jared Sturdy <sturdy@cern.ch>
2019-12-19 18:19:02 +0100

    [fixup2] remove usage of macro, fix doc comments

--- include/hw_constants_checks.h
+++ include/hw_constants_checks.h
@@ -8,20 +8,27 @@
 
 #include "hw_constants.h"
 
-/*! \brief This namespace hold the check for constants related to the GBT.
+/*!
+ *  \brief This namespace holds checks for constants related to the GBT.
  */
-namespace gbt{
-    /*! \brief This function checks the phase parameter validity.
-     *  \param[in, out] response Pointer to the RPC response object.
-     *  \param[in] phase Phase value to check.
-     *  \return Returns `false` in case of success; `true` in case of error. The precise error is logged and written to the `error` RPC key.
+namespace gbt {
+    /*!
+     *  \brief This function checks the phase parameter validity.
+     *
+     *  \throws \c std::range_error if the specified phase is outside the allowed range
+     *
+     *  \param \c phase Phase value to check
      */
-    inline bool checkPhase(uint8_t phase){
-        if (phase < PHASE_MIN)
-            EMIT_RPC_ERROR(stdsprintf("The phase parameter supplied (%hhu) is smaller than the minimal phase (%hhu).", phase, PHASE_MIN), true)
-        if (phase > PHASE_MAX)
-            EMIT_RPC_ERROR(stdsprintf("The phase parameter supplied (%hhu) is bigger than the maximal phase (%hhu).", phase, PHASE_MAX), true)
-        return false;
+    inline void checkPhase(uint8_t phase) {
+        std::stringstream errmsg;
+        if (phase < PHASE_MIN) {
+            errmsg << "The phase parameter supplied (" << phase << ") is smaller than the minimum phase (" << PHASE_MIN << ").";
+            throw std::range_error(errmsg.str());
+        } else if (phase > PHASE_MAX) {
+            errmsg << "The phase parameter supplied (" << phase << ") is bigger than the maximum phase (" << PHASE_MAX << ").";
+            throw std::range_error(errmsg.str());
+        }
+        return;
     }
 }
 
